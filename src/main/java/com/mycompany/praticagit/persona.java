e9a1c71b6670a5913ab7e6feff73d15f40447749
Luis <Luis@DESKTOP-NT9I88C>
2024-03-05 11:51:35 -0500

    Modificacion  nuevo Archivo persona

---
+++ src/main/java/com/mycompany/praticagit/persona.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.praticagit;
+
+
+public class persona {
+    
+    String Nombre;
+    String Apellido;
+    
+    
+}
